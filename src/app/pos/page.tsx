0dd7d786a5e8efebc2ab347e9961a122dd99be3b
Zakir Hussain <itsjakirhussain@gmail.com>
2024-02-06 17:33:46 +0530

    fix

--- src/app/pos/page.tsx
+++ src/app/pos/page.tsx
@@ -5,7 +5,7 @@ import SearchBar from "@/components/organisms/search-bar";
 export default function Home() {
   return (
     <>
-      <div className="grid grid-2auto-1fr  bg-paper w-full h-full md:row-span-full row-start-1 md:col-span-7 col-span-12 gap-y-4 py-4">
+      <div className="grid grid-2auto-1fr bg-paper w-full h-full md:row-span-full row-start-1 md:col-span-7 col-span-12 gap-y-4 py-4">
         <CategoriesSlide className="row-start-1 px-4" />
         <SearchBar className="row-start-2 px-4 flex md:hidden" />
         <ProductList className="row-start-3 px-4" />
