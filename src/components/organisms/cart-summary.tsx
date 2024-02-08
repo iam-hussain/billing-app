a305dcf5d4d67c9d0e2c0aa5086389b26b785329
Zakir Hussain <itsjakirhussain@gmail.com>
2024-02-09 02:06:44 +0530

    fix

--- src/components/organisms/cart-summary.tsx
+++ src/components/organisms/cart-summary.tsx
@@ -46,6 +46,9 @@ const products = [
     price: 100.0,
     image: "",
   },
+];
+
+const pendingProducts = [
   {
     name: "Chicken Noodles",
     price: 100.0,
@@ -70,73 +73,6 @@ const products = [
     price: 150.0,
     image:
       "https://images.unsplash.com/photo-1609167830220-7164aa360951?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Mutton Briyani",
-    price: 300.0,
-    image: "",
-    active: true,
-  },
-  {
-    name: "Idly",
-    price: 10.0,
-    image: "",
-  },
-  {
-    name: "Watermelon Juice",
-    price: 50.0,
-    image: "",
-  },
-  {
-    name: "Chocolate Moose",
-    price: 100.0,
-    image: "",
-  },
-  {
-    name: "Chicken Noodles",
-    price: 100.0,
-    image:
-      "https://images.unsplash.com/photo-1626804475297-41608ea09aeb?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Ghee Rava Dosa",
-    price: 40.0,
-    deck: "Neque porro quisquam est velit...",
-    image:
-      "https://images.unsplash.com/photo-1612204104655-6c8a57ae235f?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTV8fGZvb2RzfGVufDB8fDB8fHww",
-  },
-  {
-    name: "Cheese Pizza",
-    price: 150.0,
-    image:
-      "https://images.unsplash.com/photo-1506354666786-959d6d497f1a?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Peri Peri Chicken Burger",
-    price: 150.0,
-    image:
-      "https://images.unsplash.com/photo-1609167830220-7164aa360951?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  },
-  {
-    name: "Mutton Briyani",
-    price: 300.0,
-    image: "",
-    active: true,
-  },
-  {
-    name: "Idly",
-    price: 10.0,
-    image: "",
-  },
-  {
-    name: "Watermelon Juice",
-    price: 50.0,
-    image: "",
-  },
-  {
-    name: "Chocolate Moose",
-    price: 100.0,
-    image: "",
   },
   {
     name: "Chicken Noodles",
@@ -150,7 +86,14 @@ function CartSummary({ className }: { className?: string }) {
   return (
     <div className={clsx("w-full h-full cart-grid gap-4 bg-paper", className)}>
       <div className="flex justify-between align-middle items-center py-2 px-4 bg-background">
-        <p>Order ID: JJ782328</p>
+        <div className="flex text-sm flex-col">
+          {/* <div>
+            <Icon name="IoMdAdd" className="h-4 w-4" />
+          </div> */}
+          <p>Order ID: JJ782328</p>
+          <p>Table: 02</p>
+          {/* <p>Created: Today, 3:40 PM</p> */}
+        </div>
         <div className="flex justify-center align-middle gap-2">
           <Button
             className="flex justify-center align-middle gap-2"
@@ -167,37 +110,35 @@ function CartSummary({ className }: { className?: string }) {
           </Button>
         </div>
       </div>
-      <div className="flex justify-between align-middle items-center py-2 px-4 bg-background">
-        <div className="flex justify-center align-middle gap-2">
-          <ButtonToolTip
-            label="Select Table"
-            icon="MdTableRestaurant"
-            variant={"outline"}
-          />
-          <ButtonToolTip
-            label="Link Customer"
-            icon="IoPersonAddSharp"
-            variant={"outline"}
-          />
+      <div className="grid grid-cols-3 md:grid-cols-6 py-2 px-4 gap-2 bg-background">
+        <ButtonToolTip
+          label="Select Table"
+          icon="MdTableRestaurant"
+          variant={"outline"}
+        />
+        <ButtonToolTip
+          label="Link Customer"
+          icon="IoPersonAddSharp"
+          variant={"outline"}
+        />
 
-          <ButtonToolTip
-            label="Add Discount"
-            icon="TbDiscount2"
-            variant={"outline"}
-          />
+        <ButtonToolTip
+          label="Add Discount"
+          icon="TbDiscount2"
+          variant={"outline"}
+        />
 
-          <ButtonToolTip
-            label="Add Package Charge"
-            icon="PiPackageFill"
-            variant={"outline"}
-          />
+        <ButtonToolTip
+          label="Add Package Charge"
+          icon="PiPackageFill"
+          variant={"outline"}
+        />
 
-          <ButtonToolTip
-            label="Add Delivery Charge"
-            icon="TbMotorbike"
-            variant={"outline"}
-          />
-        </div>
+        <ButtonToolTip
+          label="Add Delivery Charge"
+          icon="TbMotorbike"
+          variant={"outline"}
+        />
 
         <ButtonToolTip
           label="Reset Order"
@@ -209,6 +150,44 @@ function CartSummary({ className }: { className?: string }) {
       <ScrollArea className="w-full flex justify-end grow bg-background">
         <div className="flex flex-col py-2 px-4">
           {products.map((each, i) => (
+            <>
+              <div
+                key={i}
+                className="flex justify-center items-center align-middle gap-4 rounded-md text-sm font-medium"
+              >
+                <span className="grow">{each.name}</span>
+                <div className="flex justify-center align-middle items-center text-center border border-paper">
+                  <Button className="p-1" variant={"ghost"} disabled>
+                    <Icon name="RiSubtractFill" />
+                  </Button>
+                  <span className="min-w-6">1</span>
+                  <Button className="p-1" variant={"ghost"} disabled>
+                    <Icon name="IoMdAdd" />
+                  </Button>
+                </div>
+                <span className="min-w-28 flex justify-end">
+                  ₹ {each.price.toFixed(2)}
+                </span>
+                <Button variant={"transparent"} className="p-2">
+                  <Icon name="TiDelete" className="h-6 w-6" />
+                </Button>
+              </div>
+              {products.length - 1 !== i && <Separator className={"my-2"} />}
+            </>
+          ))}
+
+          <div className="relative my-4">
+            <div className="absolute inset-0 flex items-center">
+              <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+              <span className="bg-background px-2 text-muted-foreground">
+                New Items
+              </span>
+            </div>
+          </div>
+
+          {pendingProducts.map((each, i) => (
             <>
               <div
                 key={i}
@@ -231,29 +210,29 @@ function CartSummary({ className }: { className?: string }) {
                   <Icon name="TiDelete" className="h-6 w-6" />
                 </Button>
               </div>
-              <Separator className={"my-2"} />
+              {products.length - 1 !== i && <Separator className={"my-2"} />}
             </>
           ))}
         </div>
       </ScrollArea>
       <div className="p-4 flex justify-center align-middle items-center gap-2 flex-col text-sm bg-background">
-        <div className="flex gap-2 justify-between align-middle items-center w-full px-14">
+        <div className="flex gap-2 justify-between align-middle items-center w-full md:px-14">
           <span>Subtotal</span>
           <span>₹ 1030.00</span>
         </div>
-        <div className="flex gap-2 justify-between align-middle items-center w-full px-14">
+        <div className="flex gap-2 justify-between align-middle items-center w-full md:px-14">
           <span>Tax</span>
           <span>₹ 199.00</span>
         </div>
-        <div className="flex gap-2 justify-between align-middle items-center w-full px-14">
+        <div className="flex gap-2 justify-between align-middle items-center w-full md:px-14">
           <span>Coupon</span>
           <span>₹ 0.00</span>
         </div>
-        <div className="flex gap-2 justify-between align-middle items-center w-full px-14">
+        <div className="flex gap-2 justify-between align-middle items-center w-full md:px-14">
           <span>Grand Total</span>
           <span>₹ 1300.00</span>
         </div>
-        <div className="flex gap-4 justify-between align-middle items-center w-full px-10 pt-6">
+        <div className="flex gap-4 justify-between align-middle items-center w-full md:px-10 md:pt-6 pt-2">
           <ButtonToolTip
             label="Hold Order"
             icon="MdPendingActions"
